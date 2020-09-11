9064f44900ffe51986dbb8ae3ea4859118d5f021
Brandon Squizzato <bsquizza@redhat.com>
2020-09-11 18:19:38 -0400

    Click has no attr named 'error'

--- search_replace.py
+++ search_replace.py
@@ -3,6 +3,7 @@
 import click
 import os
 import re
+import sys
 
 
 def _do_lines(search_regex, replace_regex, file_path):
@@ -19,6 +20,11 @@ def _do_file(search_regex, replace_regex, file_path):
     return re.sub(search_regex, replace_regex, data, flags=re.DOTALL)
 
 
+def _error(msg):
+    click.error(msg)
+    sys.exit(1)
+
+
 @click.command(context_settings=dict(help_option_names=["-h", "--help"]))
 @click.argument("search_regex", required=True, type=str)
 @click.argument("replace_regex", required=True, type=str)
@@ -27,11 +33,11 @@ def _do_file(search_regex, replace_regex, file_path):
 @click.option("--lines", "-l", is_flag=True, default=False, help="Find re match on each line, not the entire file at once")
 def main(search_regex, replace_regex, file_path, in_place, lines):
     if not os.path.exists(file_path):
-        click.error(f"file does not exist: {file_path}")
+        _error(f"file does not exist: {file_path}")
     if not os.path.isfile(file_path):
-        click.error(f"path is not a file: {file_path}")
+        _error(f"path is not a file: {file_path}")
     if not os.access(file_path, os.R_OK):
-        click.error(f"unable to file for reading: {file_path}")
+        _error(f"unable to file for reading: {file_path}")
 
     if lines:
         new_data = _do_lines(search_regex, replace_regex, file_path)
@@ -40,7 +46,7 @@ def main(search_regex, replace_regex, file_path, in_place, lines):
 
     if in_place:
         if not os.access(file_path, os.W_OK):
-            click.error(f"unable to file for writing: {file_path}")
+            _error(f"unable to file for writing: {file_path}")
         with open(file_path, "w") as fp:
             fp.write(new_data)
     else:
