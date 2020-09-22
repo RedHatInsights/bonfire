7f9b81730d4e494516d963f5513b57cc3c7d7e9f
Brandon Squizzato <bsquizza@redhat.com>
2020-09-22 14:45:46 -0400

    Create 'utils' dir and create_iqe_pod

---
+++ utils/create_iqe_pod.py
@@ -0,0 +1,45 @@
+import logging
+import click
+
+from bonfire.openshift import oc, wait_for_ready
+
+
+pod_yaml = """
+apiVersion: v1
+kind: Pod
+metadata:
+  name: {pod_name}
+spec:
+  containers:
+  - command:
+    - /bin/cat
+    image: quay.io/cloudservices/iqe-tests:latest
+    imagePullPolicy: Always
+    name: iqe-tests
+    resources:
+      limits:
+        cpu: "1"
+        memory: 2Gi
+      requests:
+        cpu: 500m
+        memory: 512Mi
+    stdin: true
+    tty: true
+  imagePullSecrets:
+  - name: quay-cloudservices-pull
+  restartPolicy: Never
+"""
+
+
+@click.command(context_settings=dict(help_option_names=["-h", "--help"]))
+@click.argument("namespace", type=str, required=True)
+@click.option("--pod-name", type=str, default="iqe-tests", help="name of pod (default: iqe-tests)")
+def main(namespace, pod_name):
+    logging.basicConfig(level=logging.INFO)
+    logging.getLogger("sh").setLevel(logging.CRITICAL)
+    oc("create", f="-", n=namespace, _in=pod_yaml.format(pod_name=pod_name))
+    wait_for_ready(namespace, "pod", pod_name)
+
+
+if __name__ == "__main__":
+    main()
