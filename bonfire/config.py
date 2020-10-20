75224163f33f9e141199642cddf0eb9cf573e558
Brandon Squizzato <bsquizza@redhat.com>
2020-10-20 11:39:56 -0400

    Make it an os env var

--- bonfire/config.py
+++ bonfire/config.py
@@ -15,4 +15,4 @@ PROD_ENV_NAME = os.getenv("PROD_ENV_NAME", "insights-production")
 
 ENV_NAME_FORMAT = "env-{namespace}"
 
-RECONCILE_TIMEOUT = 120
+RECONCILE_TIMEOUT = os.getenv("RECONCILE_TIMEOUT", 120)
