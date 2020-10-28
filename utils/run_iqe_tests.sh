f4699fbaece4014a24e60ba0b3850cfc60e1c7e2
Brandon Squizzato <bsquizza@redhat.com>
2020-10-28 18:11:41 -0400

    Add centralized ci scripts

---
+++ utils/run_iqe_tests.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# This script is intended to be run in an iqe-core/iqe-tests pod
+
+# Env vars required for this script:
+
+#IQE_PLUGINS=("plugin1" "plugin2")
+#IQE_MARKER_EXPRESSION="mymarker"
+#IQE_FILTER_EXPRESSION="something AND something_else"
+
+set -ex
+
+ARTIFACTS_DIR="artifacts"
+
+mkdir -p $ARTIFACTS_DIR
+
+for plugin in $IQE_PLUGINS; do
+    iqe install $plugin
+done
+
+# TODO: add vault env vars
+export ENV_FOR_DYNACONF=smoke
+
+
+for plugin in $IQE_PLUGINS; do
+    # run tests marked for 'parallel'
+    marker="parallel AND (${IQE_MARKER_EXPRESSION})"
+    iqe tests plugin ${plugin} \
+        --junitxml=${ARTIFACTS_DIR}/junit-${plugin}-parallel.xml \
+        -m \"${marker}\" \
+        -k \"${IQE_FILTER_EXPRESSION}\" \
+        -n 2
+        --log-file=${ARTIFACTS_DIR}/iqe-${plugin}-parallel.log 2>&1
+
+    # run non-parallel tests in sequence
+    marker="not parallel AND (${IQE_MARKER_EXPRESSION})"
+    iqe tests plugin ${plugin} \
+        --junitxml=${ARTIFACTS_DIR}/junit-${plugin}-sequential.xml \
+        -m \"${marker}\" \
+        -k \"${IQE_FILTER_EXPRESSION}\" \
+        --log-file=${ARTIFACTS_DIR}/iqe-${plugin}-sequential.log 2>&1
+done
