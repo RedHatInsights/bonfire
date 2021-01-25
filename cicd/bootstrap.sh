63256bc740ae8d4aa408953e4cc7b439966ff0e0
Blake Holifield <bholifie@redhat.com>
2021-01-25 15:07:02 -0600

    Check for existing bonfire (#20)
    If bonfire isn't on the system, install it; otherwise cd to cicd

--- cicd/bootstrap.sh
+++ cicd/bootstrap.sh
@@ -6,11 +6,13 @@ export GIT_COMMIT=$(git rev-parse HEAD)
 # TODO: create custom jenkins agent image that has a lot of this stuff pre-installed
 export LANG=en_US.utf-8
 export LC_ALL=en_US.utf-8
-git clone https://github.com/RedHatInsights/bonfire.git
-cd bonfire
-python3 -m venv .venv
-source .venv/bin/activate
-pip install --upgrade pip setuptools wheel
-pip install .
 
-cd cicd
+if ! (which bonfire >/dev/null); then
+    git clone https://github.com/RedHatInsights/bonfire.git
+    python3 -m venv .venv
+    source .venv/bin/activate
+    pip install --upgrade pip setuptools wheel
+    pip install ./bonfire
+fi
+
+cd bonfire/cicd
