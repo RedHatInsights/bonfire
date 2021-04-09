d6df1e67c9beaeac1975e14727815c47ee416721
Bennyturns <bennyturns@gmail.com>
2021-04-09 10:52:14 -0400

    Added PR check template (#35)
    * Added PR check template
    
    Signed-off-by: bennyturns <bturner@redhat.com>
    
    * Added example files
    
    Signed-off-by: bennyturns <bturner@redhat.com>
    
    * Added django example
    
    Signed-off-by: bennyturns <bturner@redhat.com>
    
    * Fixes for squiz from review
    
    Signed-off-by: bennyturns <bturner@redhat.com>
    
    * A few tweaks, move to 'examples' dir
    
    Co-authored-by: bennyturns <bturner@redhat.com>
    Co-authored-by: Brandon Squizzato <bsquizza@redhat.com>

---
+++ cicd/examples/pr_check_template.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+# --------------------------------------------
+# Options that must be configured by app owner
+# --------------------------------------------
+APP_NAME="CHANGEME"  # name of app-sre "application" folder this component lives in
+COMPONENT_NAME="CHANGEME"  # name of app-sre "resourceTemplate" in deploy.yaml for this component
+IMAGE="quay.io/cloudservices/CHANGEME"  # image location on quay
+
+IQE_PLUGINS="CHANGEME"  # name of the IQE plugin for this app.
+IQE_MARKER_EXPRESSION="CHANGEME"  # This is the value passed to pytest -m
+IQE_FILTER_EXPRESSION=""  # This is the value passed to pytest -k
+
+# Install bonfire repo/initialize
+CICD_URL=https://raw.githubusercontent.com/RedHatInsights/bonfire/master/cicd
+curl -s $CICD_URL/bootstrap.sh -o bootstrap.sh
+
+# The contents of the bootstrap script can be found here:
+# https://raw.githubusercontent.com/RedHatInsights/bonfire/master/cicd/bootstrap.sh
+# This script automates the install / config of bonfire, the DOCs covering this can be found:
+# https://internal.cloud.redhat.com/docs/devprod/ephemeral/01-onboarding/
+source bootstrap.sh  # checks out bonfire and changes to "cicd" dir...
+
+# The contents of build.sh can be found at:
+# https://raw.githubusercontent.com/RedHatInsights/bonfire/master/cicd/build.sh
+# This script is used to build the image that is used in the PR Check
+source build.sh
+
+# Your APP's unit tests should be run in the unit_test.sh script.  Two different
+# examples of unit_test.sh are provided in:
+# https://raw.githubusercontent.com/RedHatInsights/bonfire/master/cicd/examples/
+#
+# One of these scripts should be choosen based on your APP's architecture, modified, and placed
+# in your APP's git repository.  The ephemeral DB example is for when the unit tests require a
+# real DB, the other is for a more traditional unit test where everything runs self-contained.
+#
+# One thing to note is that the unit test run results are expected to be in a junit XML format,
+# in the examples we demonstrate how to create a 'dummy result file' as a temporary work-around.
+source unit_test.sh
+
+# The contents of this script can be found at:
+# https://raw.githubusercontent.com/RedHatInsights/bonfire/master/cicd/deploy_ephemeral_env.sh
+# This script is used to deploy the ephemeral environment for smoke tests.
+# The manual steps for this can be found in:
+# https://internal.cloud.redhat.com/docs/devprod/ephemeral/02-deploying/
+source deploy_ephemeral_env.sh
+
+# The contents of this script can be found at:
+# https://raw.githubusercontent.com/RedHatInsights/bonfire/master/cicd/smoke_test.sh
+# This script is used to run the smoke tests for a given APP.  The ENV VARs are
+# defined at the top in the "Options that must be configured by app owner" section
+# will control the behavior of the test.
+source smoke_test.sh
---
+++ cicd/examples/unit_test_example.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# Change dir to the APP source code
+cd $APP_ROOT
+
+# Add you unit test specific code
+export GO111MODULE="on"
+export GOPATH=/var/gopath
+
+# go get stuff...
+
+# If your app requires a 'cdappconfig.json' when running unit tests, create a dummy cdappconfig
+# that has appropraite values and.  Store this file in your git repo.  Example can be found here:
+# https://github.com/RedHatInsights/insights-ingress-go/blob/master/cdappconfig.json
+ACG_CONFIG="$(pwd)/cdappconfig.json"  go test -v -race -coverprofile=coverage.txt -covermode=atomic ./...
+
+# If the return code is not 0, exit with an error
+if [ $? != 0 ]; then
+    exit $?
+else
+    # If your unit tests store junit xml results, you should store them in a file matching format `artifacts/junit-*.xml`
+    # If you have no junit file, use the below code to create a 'dummy' result file so Jenkins will not fail
+    mkdir -p artifacts
+    cat << EOF > artifacts/junit-dummy.xml
+    <testsuite tests="1">
+        <testcase classname="dummy" name="dummytest"/>
+    </testsuite>
+EOF
+fi
+
+# Make sure to 'cd -' at the end to return to the original directory
+cd -
