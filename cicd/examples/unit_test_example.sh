2e579a44494f6d9db6f7bd8a88bd49682f16492e
Brandon Squizzato <bsquizza@redhat.com>
2021-04-09 15:35:16 -0400

    Use abs path for artifacts

--- cicd/examples/unit_test_example.sh
+++ cicd/examples/unit_test_example.sh
@@ -18,7 +18,7 @@ else
     # If your unit tests store junit xml results, you should store them in a file matching format `artifacts/junit-*.xml`
     # If you have no junit file, use the below code to create a 'dummy' result file so Jenkins will not fail
     mkdir -p artifacts
-    cat << EOF > artifacts/junit-dummy.xml
+    cat << EOF > $WORKSPACE/artifacts/junit-dummy.xml
     <testsuite tests="1">
         <testcase classname="dummy" name="dummytest"/>
     </testsuite>
