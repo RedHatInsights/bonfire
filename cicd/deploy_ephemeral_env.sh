198ecb94764380869cb9cf21c62cfed7712f7c08
Patrik Segedy <psegedy@redhat.com>
2021-04-30 17:22:55 +0200

    Make sure NAMESPACE is set when deploy fails (#31)

--- cicd/deploy_ephemeral_env.sh
+++ cicd/deploy_ephemeral_env.sh
@@ -3,13 +3,11 @@ source ${CICD_ROOT}/_common_deploy_logic.sh
 # Deploy k8s resources for app and its dependencies (use insights-stage instead of insights-production for now)
 # -> use this PR as the template ref when downloading configurations for this component
 # -> use this PR's newly built image in the deployed configurations
-result=$(bonfire deploy \
+export NAMESPACE=$(bonfire namespace reserve)
+bonfire deploy \
     ${APP_NAME} \
     --source=appsre \
     --ref-env insights-stage \
     --set-template-ref ${APP_NAME}/${COMPONENT_NAME}=${GIT_COMMIT} \
-    --set-image-tag ${IMAGE}=${IMAGE_TAG})
-
-if [ $? -eq 0 ]; then
-    export NAMESPACE=$result
-fi
+    --set-image-tag ${IMAGE}=${IMAGE_TAG} \
+    --namespace ${NAMESPACE}
