6c94eab9acfd4cbc7e09ecf09e1babb5bda843ee
bholifie <bholifie@redhat.com>
2021-07-07 15:33:51 -0500

    use new file to source for cji

---
+++ cicd/cji_smoke_test.sh
@@ -0,0 +1,52 @@
+# Run smoke tests as a ClowdJobInvocation deployed by bonfire
+
+# Env vars defined by caller:
+#IQE_PLUGINS="plugin1,plugin2" -- pytest plugins to run separated by "," #IQE_MARKER_EXPRESSION="mymarker" -- pytest marker expression
+#IQE_FILTER_EXPRESSION="something AND something_else" -- pytest filter, can be "" if no filter desired
+#NAMESPACE="mynamespace" -- namespace to deploy iqe pod into, can be set by 'deploy_ephemeral_env.sh'
+
+CJI_NAME="$APP_NAME-smoke-tests"
+
+function kill_port_fwd {
+    echo "Caught signal, kill port forward"
+    if [ ! -z "$PORT_FORWARD_PID" ]; then kill $PORT_FORWARD_PID; fi
+}
+
+# Invoke the CJI with the exported vars from pr_check
+pod=$(bonfire deploy-iqe-cji $APP_NAME -m $IQE_MARKER_EXPRESSION -e "clowder_smoke" --cji-name $CJI_NAME -n $NAMESPACE)
+
+# Pipe logs to background to keep them rolling in jenkins
+oc logs -n $NAMESPACE $pod -f &
+
+# Wait for the job to Complete or Fail before we try to grab artifacts
+# condition=complete does trigger when the job fails
+oc wait --timeout=$IQE_CJI_TIMEOUT --for=condition=Complete -n $NAMESPACE job/$CJI_NAME-iqe
+
+# Get the minio client
+curl https://dl.min.io/client/mc/release/linux-amd64/mc -o mc
+chmod +x mc
+
+# Set up port-forward for minio
+LOCAL_SVC_PORT=$(python -c 'import socket; s=socket.socket(); s.bind(("", 0)); print(s.getsockname()[1]); s.close()')
+oc port-forward svc/env-$NAMESPACE-minio $LOCAL_SVC_PORT:9000 -n $NAMESPACE &
+sleep 5
+PORT_FORWARD_PID=$!
+trap "teardown" EXIT ERR SIGINT SIGTERM
+
+# Get the secret from the env
+oc get secret env-$NAMESPACE-minio -o json -n $NAMESPACE | jq -r '.data' > minio-creds.json
+
+# Grab the needed creds from the secret
+export MINIO_ACCESS=$(jq -r .accessKey < minio-creds.json | base64 -d)
+export MINIO_SECRET_KEY=$(jq -r .secretKey < minio-creds.json | base64 -d)
+export MINIO_HOST=localhost
+export MINIO_PORT=$LOCAL_SVC_PORT
+
+# Setup the minio client to auth to the local eph minio in the ns
+./mc alias set minio http://$MINIO_HOST:$MINIO_PORT $MINIO_ACCESS $MINIO_SECRET_KEY
+
+# "mirror" copies the entire artifacts dir from the pod and writes it to the jenkins node
+./mc mirror --overwrite minio/$pod-artifacts artifacts/
+
+echo "copied artifacts from iqe pod: "
+ls -l $WORKSPACE/artifacts
--- cicd/smoke_test.sh
+++ cicd/smoke_test.sh
@@ -1,92 +1,32 @@
 # Spin up iqe pod and execute IQE tests in it
 
 # Env vars defined by caller:
-#IQE_PLUGINS="plugin1,plugin2" -- pytest plugins to run separated by "," #IQE_MARKER_EXPRESSION="mymarker" -- pytest marker expression
+#IQE_PLUGINS="plugin1,plugin2" -- pytest plugins to run separated by ","
+#IQE_MARKER_EXPRESSION="mymarker" -- pytest marker expression
 #IQE_FILTER_EXPRESSION="something AND something_else" -- pytest filter, can be "" if no filter desired
 #NAMESPACE="mynamespace" -- namespace to deploy iqe pod into, can be set by 'deploy_ephemeral_env.sh'
 
+IQE_POD_NAME="iqe-tests"
 
-function kill_port_fwd {
-    echo "Caught signal, kill port forward"
-    if [ ! -z "$PORT_FORWARD_PID" ]; then kill $PORT_FORWARD_PID; fi
-}
-
-# The CJI var name will need to be exported in the main pr_check.sh
-oc apply -n $NAMESPACE -f $APP_ROOT/$CJI_PATH
-
-job_name=$APP_NAME-smoke-tests-iqe
-found=false
-end=$((SECONDS+60))
-
-echo "Waiting for Job $job_name to appear"
-
-while [ $SECONDS -lt $end ]; do
-    if `oc get job $job_name -n $NAMESPACE >/dev/null 2>&1`; then
-        found=true
-        break
-    fi
-    sleep 1
-done
-
-if [ "$found" == "false" ] ; then
-    echo "Job $job_name failed to appear"
-    exit 1
+# create a custom svc acct for the iqe pod to run with that has elevated permissions
+SA=$(oc get -n $NAMESPACE sa iqe --ignore-not-found -o jsonpath='{.metadata.name}')
+if [ -z "$SA" ]; then
+    oc create -n $NAMESPACE sa iqe
 fi
+oc policy -n $NAMESPACE add-role-to-user edit system:serviceaccount:$NAMESPACE:iqe
+oc secrets -n $NAMESPACE link iqe quay-cloudservices-pull --for=pull,mount
 
-echo "Waiting for Job $job_name to be running"
-running=false
-pod=""
+python $CICD_ROOT/iqe_pod/create_iqe_pod.py $NAMESPACE \
+    -e IQE_PLUGINS=$IQE_PLUGINS \
+    -e IQE_MARKER_EXPRESSION=$IQE_MARKER_EXPRESSION \
+    -e IQE_FILTER_EXPRESSION=$IQE_FILTER_EXPRESSION \
+    -e ENV_FOR_DYNACONF=smoke \
+    -e NAMESPACE=$NAMESPACE
 
-# The jq magic will find all running pods in the ns and regex on the app name
-# Loop over for SECONDS and send back the pod's name once found
-while [ $SECONDS -lt $end ]; do
-    pod=$(oc get pods -n $NAMESPACE -o json | jq -r --arg JOB $job_name '.items[] | select(.status.phase=="Running") | select(.metadata.name|test($JOB)) .metadata.name')
-    if [[ -n $pod ]]; then
-        running=true
-        break
-    fi
-    sleep 5
-done
+oc cp -n $NAMESPACE $CICD_ROOT/iqe_pod/iqe_runner.sh $IQE_POD_NAME:/iqe_venv/iqe_runner.sh
+oc exec $IQE_POD_NAME -n $NAMESPACE -- bash /iqe_venv/iqe_runner.sh
 
-if [ "$running" == "false" ] ; then
-    echo "Job $job_name failed to start"
-    exit 1
-fi
-
-# Pipe logs to background to keep them rolling in jenkins
-oc logs -n $NAMESPACE $pod -f &
-
-# Wait for the job to Complete or Fail before we try to grab artifacts
-# condition=complete does trigger when the job fails
-oc wait --timeout=3m --for=condition=Complete -n $NAMESPACE job/$job_name
-
-# Get the minio client
-curl https://dl.min.io/client/mc/release/linux-amd64/mc -o mc
-chmod +x mc
-
-# Set up port-forward for minio
-LOCAL_SVC_PORT=$(python -c 'import socket; s=socket.socket(); s.bind(("", 0)); print(s.getsockname()[1]); s.close()')
-oc port-forward svc/env-$NAMESPACE-minio $LOCAL_SVC_PORT:9000 -n $NAMESPACE &
-PORT_FORWARD_PID=$!
-trap "teardown" EXIT ERR SIGINT SIGTERM
-
-# sleep so port-forward has time to get established
-sleep 5
-
-# Get the secret from the env
-oc get secret env-$NAMESPACE-minio -o json -n $NAMESPACE | jq -r '.data' > minio-creds.json
-
-# Grab the needed creds from the secret
-export MINIO_ACCESS=$(jq -r .accessKey < minio-creds.json | base64 -d)
-export MINIO_SECRET_KEY=$(jq -r .secretKey < minio-creds.json | base64 -d)
-export MINIO_HOST=localhost
-export MINIO_PORT=$LOCAL_SVC_PORT
-
-# Setup the minio client to auth to the local eph minio in the ns
-./mc alias set minio http://$MINIO_HOST:$MINIO_PORT $MINIO_ACCESS $MINIO_SECRET_KEY
-
-# "mirror" copies the entire artifacts dir from the pod and writes it to the jenkins node
-./mc mirror --overwrite minio/$pod-artifacts artifacts/
+oc cp -n $NAMESPACE $IQE_POD_NAME:artifacts/ $WORKSPACE/artifacts
 
 echo "copied artifacts from iqe pod: "
 ls -l $WORKSPACE/artifacts
