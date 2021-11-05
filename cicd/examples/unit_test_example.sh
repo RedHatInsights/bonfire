5b82ef20e7c2eafe6a1dfe07fc8e7e115ea0e89a
Brandon Squizzato <bsquizza@redhat.com>
2021-11-05 12:56:52 -0400

    Show how to move back to bonfire venv properly

--- cicd/examples/unit_test_example.sh
+++ cicd/examples/unit_test_example.sh
@@ -16,6 +16,9 @@ set -e
 
 # Evaluate the test result.
 
+# If you set up a python virtual env for your tests, move back to the bonfire virtual env...
+source .bonfire_venv/bin/activate
+
 # If your unit tests store junit xml results, you should store them in a file matching format `artifacts/junit-*.xml`
 # If you have no junit file, use the below code to create a 'dummy' result file so Jenkins will not fail
 mkdir -p $WORKSPACE/artifacts
