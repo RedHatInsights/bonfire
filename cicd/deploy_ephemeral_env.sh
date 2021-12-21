2cff5a3cabb4db26fe61b26599ae2a066be2a883
Brandon Squizzato <bsquizza@redhat.com>
2021-12-21 11:52:44 -0500

    Add EXTRA_DEPLOY_ARGS

--- cicd/deploy_ephemeral_env.sh
+++ cicd/deploy_ephemeral_env.sh
@@ -1,5 +1,8 @@
 source ${CICD_ROOT}/_common_deploy_logic.sh
 
+# Caller can define any extra deploy arguments to be passed to bonfire
+: "${EXTRA_DEPLOY_ARGS:='""'}"
+
 # Deploy k8s resources for app and its dependencies (use insights-stage instead of insights-production for now)
 # -> use this PR as the template ref when downloading configurations for this component
 # -> use this PR's newly built image in the deployed configurations
@@ -14,5 +17,6 @@ bonfire deploy \
     --namespace ${NAMESPACE} \
     --timeout ${DEPLOY_TIMEOUT} \
     ${COMPONENTS_ARG} \
-    ${COMPONENTS_RESOURCES_ARG}
+    ${COMPONENTS_RESOURCES_ARG} \
+    ${EXTRA_DEPLOY_ARGS}
 set +x
