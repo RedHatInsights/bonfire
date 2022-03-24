d7ceb2659e3d4eaaae7c4bdc10a3f82c9710c407
Matthew Knop <mknop@mknop.remote.csb>
2022-03-24 15:26:57 -0600

    Added unit tests for utils functions.

---
+++ tests/test_utils.py
@@ -0,0 +1,51 @@
+import pytest
+from bonfire.utils import (
+    validate_time_string,
+    hms_to_seconds
+)
+
+
+@pytest.mark.parametrize(
+    "time, expected",
+    [
+        (
+            "30m",
+            "30m"
+        ),
+        (
+            "1h45m",
+            "1h45m"
+        ),
+        (
+            "1h20s",
+            "1h20s"
+        ),
+    ]
+)
+def test_validate_time_string(time: str, expected: str):
+    result = validate_time_string(time)
+
+    assert result == expected
+
+
+@pytest.mark.parametrize(
+    "seconds, expected",
+    [
+        (
+            "1h",
+            3600,
+        ),
+        (
+            "45m",
+            2700
+        ),
+        (
+            "65s",
+            65
+        ),
+    ]
+)
+def test_hms_to_seconds(seconds: str, expected: int):
+    result = hms_to_seconds(seconds)
+
+    assert result == expected
