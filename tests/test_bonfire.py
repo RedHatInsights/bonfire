e6e355ea50eb5a463a568d1725c0f0d4833f27d3
Matthew Knop <mknop@mknop.remote.csb>
2022-03-30 12:05:25 -0600

    Added tests for --duration flag.

--- tests/test_bonfire.py
+++ tests/test_bonfire.py
@@ -67,3 +67,37 @@ def test_ns_reserve_options_requester(mocker, caplog, user, expected):
     result = runner.invoke(bonfire.namespace, ["reserve", "--requester", user])
 
     assert result.output.rstrip() == expected
+
+
+@pytest.mark.parametrize(
+    "duration, expected",
+    [
+        (
+            "1h", "1h"
+        ),
+        (
+            None, "1h"
+        ),
+        (
+            "30m", "30m"
+        ),
+    ]
+)
+def test_ns_reserve_options_duration(mocker, caplog, duration, expected):
+    caplog.set_level(100000)
+
+    ns = Mock()
+    ns.name=expected
+    mocker.patch('bonfire.bonfire.has_ns_operator', return_value=True)
+    mocker.patch('bonfire.openshift.has_ns_operator', return_value=True)
+    mocker.patch('bonfire.openshift.get_api_resources', return_value={"name": duration})
+    mocker.patch('bonfire.openshift.check_for_existing_reservation', return_value=True)
+    mocker.patch('bonfire.openshift.parse_restype', return_value="")
+    mocker.patch('bonfire.openshift.get_json', return_value="")
+    mocker.patch('bonfire.openshift.get_all_reservations', return_value="")
+    mocker.patch('bonfire.bonfire.reserve_namespace', return_value=ns)
+    
+    runner = CliRunner()
+    result = runner.invoke(bonfire.namespace, ["reserve", "--requester", duration])
+    
+    assert result.output.rstrip() == expected
