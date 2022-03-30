40771ad0d7e13f8689337d3757890ecd463fc8ad
Matthew Knop <mknop@mknop.remote.csb>
2022-03-30 11:59:20 -0600

    Added tests for --requester flag.

--- tests/test_bonfire.py
+++ tests/test_bonfire.py
@@ -18,13 +18,14 @@ from bonfire import openshift
         ),
     ]
 )
-def test_ns_reserve_options_name(mocker, name, expected):
+def test_ns_reserve_options_name(mocker, caplog, name, expected):
+    caplog.set_level(100000) 
+
     ns = Mock()
     ns.name=expected
     mocker.patch('bonfire.bonfire.has_ns_operator', return_value=True)
     mocker.patch('bonfire.openshift.has_ns_operator', return_value=True)
     mocker.patch('bonfire.openshift.get_api_resources', return_value={"name": name})
-    #mocker.patch('bonfire.bonfire.reserve_namespace', return_value={"name": expected})
     mocker.patch('bonfire.openshift.check_for_existing_reservation', return_value=True)
     mocker.patch('bonfire.openshift.parse_restype', return_value="")
     mocker.patch('bonfire.openshift.get_json', return_value="")
@@ -33,5 +34,36 @@ def test_ns_reserve_options_name(mocker, name, expected):
     
     runner = CliRunner()
     result = runner.invoke(bonfire.namespace, ["reserve", "--name", name])
+
+    assert result.output.rstrip() == expected
+
+
+@pytest.mark.parametrize(
+    "user, expected",
+    [
+        (
+            "user1", "user1"
+        ),
+        (
+            "user2", "user2"
+        ),
+    ]
+)
+def test_ns_reserve_options_requester(mocker, caplog, user, expected):
+    caplog.set_level(100000)
+
+    ns = Mock()
+    ns.name=expected
+    mocker.patch('bonfire.bonfire.has_ns_operator', return_value=True)
+    mocker.patch('bonfire.openshift.has_ns_operator', return_value=True)
+    mocker.patch('bonfire.openshift.get_api_resources', return_value={"name": user})
+    mocker.patch('bonfire.openshift.check_for_existing_reservation', return_value=True)
+    mocker.patch('bonfire.openshift.parse_restype', return_value="")
+    mocker.patch('bonfire.openshift.get_json', return_value="")
+    mocker.patch('bonfire.openshift.get_all_reservations', return_value="")
+    mocker.patch('bonfire.bonfire.reserve_namespace', return_value=ns)
+    
+    runner = CliRunner()
+    result = runner.invoke(bonfire.namespace, ["reserve", "--requester", user])
 
     assert result.output.rstrip() == expected
