39fac1ee7f2a7ea5b8a66581ab492ce6f47c762a
Brandon Squizzato <bsquizza@redhat.com>
2022-04-08 10:50:13 -0400

    Install bonfire v4

--- cicd/bootstrap.sh
+++ cicd/bootstrap.sh
@@ -63,7 +63,7 @@ python3 -m venv .bonfire_venv
 source .bonfire_venv/bin/activate
 
 pip install --upgrade pip 'setuptools<58' wheel
-pip install --upgrade 'crc-bonfire>=3.8.6'
+pip install --upgrade 'crc-bonfire>=4.0.0'
 
 # clone repo to download cicd scripts
 rm -fr $BONFIRE_ROOT
