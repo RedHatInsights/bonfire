ba8271ab862fe573dbec802460ce50fcbf672d7e
Matt Knop <mknop@redhat.com>
2022-04-14 15:56:12 -0600

    Black formatting.

--- tests/test_bonfire.py
+++ tests/test_bonfire.py
@@ -13,12 +13,13 @@ DATA_PATH = Path(__file__).parent.joinpath("data")
 def namespace_list() -> list:
     with open(DATA_PATH.joinpath("namespace_data.json"), "r") as namespace_data_file:
         return json.load(namespace_data_file)["items"]
-    
 
 
 @pytest.fixture(scope="module")
 def reservation_list() -> list:
-    with open(DATA_PATH.joinpath("reservation_data.json"), "r") as reservation_data_file:
+    with open(
+        DATA_PATH.joinpath("reservation_data.json"), "r"
+    ) as reservation_data_file:
         return json.load(reservation_data_file)["items"]
 
 
@@ -93,9 +94,13 @@ def test_ns_reserve_options_duration(mocker, caplog, duration: str):
     runner.invoke(bonfire.namespace, ["reserve", "--duration", duration])
 
     if duration:
-        mock_process_reservation.assert_called_once_with(None, "user-3", duration, local=True)
+        mock_process_reservation.assert_called_once_with(
+            None, "user-3", duration, local=True
+        )
     else:
-        mock_process_reservation.assert_called_once_with(None, "user-3", "1h", local=True)
+        mock_process_reservation.assert_called_once_with(
+            None, "user-3", "1h", local=True
+        )
 
 
 def test_ns_list_option(mocker, caplog, namespace_list: list, reservation_list: list):
@@ -104,7 +109,9 @@ def test_ns_list_option(mocker, caplog, namespace_list: list, reservation_list: 
     mocker.patch("bonfire.bonfire.has_ns_operator", return_value=True)
     mocker.patch("bonfire.namespaces.get_all_namespaces", return_value=namespace_list)
     mocker.patch("bonfire.namespaces.get_json", return_value={})
-    mocker.patch("bonfire.namespaces.get_all_reservations", return_value=reservation_list)
+    mocker.patch(
+        "bonfire.namespaces.get_all_reservations", return_value=reservation_list
+    )
     mocker.patch("bonfire.namespaces.on_k8s", return_value=False)
     mocker.patch("bonfire.namespaces.whoami", return_value="user-1")
     mocker.patch("bonfire.openshift.process_template", return_value={})
@@ -121,13 +128,17 @@ def test_ns_list_option(mocker, caplog, namespace_list: list, reservation_list: 
     assert " ".join(["namespace-5", "true", "false", "none", "user-5"]) in actual
 
 
-def test_ns_list_options_available(mocker, caplog, namespace_list: list, reservation_list: list):
+def test_ns_list_options_available(
+    mocker, caplog, namespace_list: list, reservation_list: list
+):
     caplog.set_level(100000)
 
     mocker.patch("bonfire.bonfire.has_ns_operator", return_value=True)
     mocker.patch("bonfire.namespaces.get_all_namespaces", return_value=namespace_list)
     mocker.patch("bonfire.namespaces.get_json", return_value={})
-    mocker.patch("bonfire.namespaces.get_all_reservations", return_value=reservation_list)
+    mocker.patch(
+        "bonfire.namespaces.get_all_reservations", return_value=reservation_list
+    )
     mocker.patch("bonfire.namespaces.on_k8s", return_value=False)
     mocker.patch("bonfire.namespaces.whoami", return_value="user-1")
     mocker.patch("bonfire.openshift.process_template", return_value={})
@@ -144,13 +155,17 @@ def test_ns_list_options_available(mocker, caplog, namespace_list: list, reserva
     assert " ".join(["namespace-5", "true", "false", "none", "user-5"]) not in actual
 
 
-def test_ns_list_option_mine(mocker, caplog, namespace_list: list, reservation_list: list):
+def test_ns_list_option_mine(
+    mocker, caplog, namespace_list: list, reservation_list: list
+):
     caplog.set_level(100000)
 
     mocker.patch("bonfire.bonfire.has_ns_operator", return_value=True)
     mocker.patch("bonfire.namespaces.get_all_namespaces", return_value=namespace_list)
     mocker.patch("bonfire.namespaces.get_json", return_value={})
-    mocker.patch("bonfire.namespaces.get_all_reservations", return_value=reservation_list)
+    mocker.patch(
+        "bonfire.namespaces.get_all_reservations", return_value=reservation_list
+    )
     mocker.patch("bonfire.namespaces.on_k8s", return_value=False)
     mocker.patch("bonfire.namespaces.whoami", return_value="user-1")
     mocker.patch("bonfire.openshift.process_template", return_value={})
