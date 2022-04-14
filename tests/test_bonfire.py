7a6640093fb009198ed099d8201d08a30003fa9a
Matthew A. Knop <knopmatthew93@gmail.com>
2022-04-14 14:55:21 -0600

    Add unit tests for Bonfire.py (#201)
    * Added unit tests for utils functions and bonfire namespace CLI options
    
    Co-authored-by: Matthew Knop <mknop@mknop.remote.csb>
    Co-authored-by: Matt Knop <mknop@redhat.com>
    Co-authored-by: Brandon Squizzato <bsquizza@redhat.com>

---
+++ tests/test_bonfire.py
@@ -0,0 +1,154 @@
+import pytest
+from click.testing import CliRunner
+from pathlib import Path
+import json
+
+from bonfire import bonfire
+
+
+DATA_PATH = Path(__file__).parent.joinpath("data")
+
+
+@pytest.fixture(scope="module")
+def namespace_list() -> list:
+    with open(DATA_PATH.joinpath("namespace_data.json"), "r") as namespace_data_file:
+        return json.load(namespace_data_file)["items"]
+
+
+@pytest.fixture(scope="module")
+def reservation_list() -> list:
+    with open(DATA_PATH.joinpath("reservation_data.json"), "r") as reservation_data_file:
+        return json.load(reservation_data_file)["items"]
+
+
+@pytest.mark.parametrize(
+    "name",
+    [
+        ("namespace-6"),
+        ("namespace-7"),
+    ],
+)
+def test_ns_reserve_options_name(mocker, name: str):
+    mocker.patch("bonfire.bonfire.has_ns_operator", return_value=True)
+    mocker.patch("bonfire.bonfire._get_requester", return_value="user-3")
+    mocker.patch("bonfire.bonfire.check_for_existing_reservation", return_value=False)
+    mocker.patch("bonfire.namespaces.get_reservation", return_value=None)
+    mocker.patch("bonfire.openshift.process_template", return_value={})
+
+    mock_process_reservation = mocker.patch("bonfire.namespaces.process_reservation")
+
+    runner = CliRunner()
+    runner.invoke(bonfire.namespace, ["reserve", "--name", name])
+
+    mock_process_reservation.assert_called_once_with(name, "user-3", "1h", local=True)
+
+
+@pytest.mark.parametrize(
+    "requester",
+    [
+        ("user-3"),
+        ("user-2"),
+    ],
+)
+def test_ns_reserve_options_requester(mocker, requester: str):
+    mocker.patch("bonfire.bonfire.has_ns_operator", return_value=True)
+    mocker.patch("bonfire.bonfire._get_requester", return_value=requester)
+    mocker.patch("bonfire.bonfire.check_for_existing_reservation", return_value=False)
+    mocker.patch("bonfire.namespaces.get_reservation", return_value=None)
+    mocker.patch("bonfire.openshift.process_template", return_value={})
+
+    mock_process_reservation = mocker.patch("bonfire.namespaces.process_reservation")
+
+    runner = CliRunner()
+    runner.invoke(bonfire.namespace, ["reserve", "--requester", requester])
+
+    mock_process_reservation.assert_called_once_with(None, requester, "1h", local=True)
+
+
+@pytest.mark.parametrize(
+    "duration",
+    [
+        ("1h"),
+        (None),
+        ("30m"),
+    ],
+)
+def test_ns_reserve_options_duration(mocker, duration: str):
+    mocker.patch("bonfire.bonfire.has_ns_operator", return_value=True)
+    mocker.patch("bonfire.bonfire._get_requester", return_value="user-3")
+    mocker.patch("bonfire.bonfire.check_for_existing_reservation", return_value=False)
+    mocker.patch("bonfire.namespaces.get_reservation", return_value=None)
+    mocker.patch("bonfire.openshift.process_template", return_value={})
+
+    mock_process_reservation = mocker.patch("bonfire.namespaces.process_reservation")
+
+    runner = CliRunner()
+    runner.invoke(bonfire.namespace, ["reserve", "--duration", duration])
+
+    if duration:
+        mock_process_reservation.assert_called_once_with(None, "user-3", duration, local=True)
+    else:
+        mock_process_reservation.assert_called_once_with(None, "user-3", "1h", local=True)
+
+
+def test_ns_list_option(mocker, namespace_list: list, reservation_list: list):
+    mocker.patch("bonfire.bonfire.has_ns_operator", return_value=True)
+    mocker.patch("bonfire.namespaces.get_all_namespaces", return_value=namespace_list)
+    mocker.patch("bonfire.namespaces.get_json", return_value={})
+    mocker.patch("bonfire.namespaces.get_all_reservations", return_value=reservation_list)
+    mocker.patch("bonfire.namespaces.on_k8s", return_value=False)
+    mocker.patch("bonfire.namespaces.whoami", return_value="user-1")
+    mocker.patch("bonfire.openshift.process_template", return_value={})
+
+    runner = CliRunner()
+    result = runner.invoke(bonfire.namespace, ["list"])
+
+    actual = " ".join(result.output.split())
+
+    assert " ".join(["namespace-1", "true", "false", "none", "user-1"]) in actual
+    assert " ".join(["namespace-2", "true", "false", "none", "user-2"]) in actual
+    assert " ".join(["namespace-3", "false", "ready", "none"]) in actual
+    assert " ".join(["namespace-4", "false", "ready", "none"]) in actual
+    assert " ".join(["namespace-5", "true", "false", "none", "user-5"]) in actual
+
+
+def test_ns_list_options_available(mocker, namespace_list: list, reservation_list: list):
+    mocker.patch("bonfire.bonfire.has_ns_operator", return_value=True)
+    mocker.patch("bonfire.namespaces.get_all_namespaces", return_value=namespace_list)
+    mocker.patch("bonfire.namespaces.get_json", return_value={})
+    mocker.patch("bonfire.namespaces.get_all_reservations", return_value=reservation_list)
+    mocker.patch("bonfire.namespaces.on_k8s", return_value=False)
+    mocker.patch("bonfire.namespaces.whoami", return_value="user-1")
+    mocker.patch("bonfire.openshift.process_template", return_value={})
+
+    runner = CliRunner()
+    result = runner.invoke(bonfire.namespace, ["list", "--available"])
+
+    actual = " ".join(result.output.split())
+
+    assert " ".join(["namespace-1", "true", "false", "none", "user-1"]) not in actual
+    assert " ".join(["namespace-2", "true", "false", "none", "user-2"]) not in actual
+    assert " ".join(["namespace-3", "false", "ready", "none"]) in actual
+    assert " ".join(["namespace-4", "false", "ready", "none"]) in actual
+    assert " ".join(["namespace-5", "true", "false", "none", "user-5"]) not in actual
+
+
+def test_ns_list_option_mine(mocker, namespace_list: list, reservation_list: list):
+    mocker.patch("bonfire.bonfire.has_ns_operator", return_value=True)
+    mocker.patch("bonfire.namespaces.get_all_namespaces", return_value=namespace_list)
+    mocker.patch("bonfire.namespaces.get_json", return_value={})
+    mocker.patch("bonfire.namespaces.get_all_reservations", return_value=reservation_list)
+    mocker.patch("bonfire.namespaces.on_k8s", return_value=False)
+    mocker.patch("bonfire.namespaces.whoami", return_value="user-1")
+    mocker.patch("bonfire.openshift.process_template", return_value={})
+
+    runner = CliRunner()
+    result = runner.invoke(bonfire.namespace, ["list", "--mine"])
+
+    actual = " ".join(result.output.split())
+
+    assert " ".join(["namespace-1", "true", "false", "none", "user-1"]) in actual
+    assert " ".join(["namespace-2", "true", "false", "none", "user-2"]) not in actual
+    assert " ".join(["namespace-3", "false", "ready", "none"]) not in actual
+    assert " ".join(["namespace-4", "false", "ready", "none"]) not in actual
+    assert " ".join(["namespace-5", "true", "false", "none", "user-5"]) not in actual
---
+++ tests/test_utils.py
@@ -0,0 +1,70 @@
+import pytest
+
+from bonfire.utils import split_equals, validate_time_string, get_version, hms_to_seconds
+
+
+@pytest.mark.parametrize(
+    "list_of_str, expected",
+    [
+        (["t1=test1", "t2=test2"], {"t1": "test1", "t2": "test2"}),
+        (["t3=test3", "t4=test4"], {"t3": "test3", "t4": "test4"}),
+    ],
+)
+def test_split_equals_pass(list_of_str: str, expected: str):
+    result = split_equals(list_of_str)
+
+    assert result == expected
+
+
+@pytest.mark.parametrize(
+    "list_of_str",
+    [
+        (["t1 = test1", "t2 = test2"],),
+        (["t1 test1", "t2 test2"],),
+    ],
+)
+def test_split_equals_raises_error(list_of_str: list):
+    with pytest.raises(ValueError):
+        split_equals(list_of_str)
+
+
+@pytest.mark.parametrize(
+    "time, expected",
+    [
+        ("30m", "30m"),
+        ("1h45m", "1h45m"),
+        ("1h20s", "1h20s"),
+        ("2h15m32s", "2h15m32s"),
+    ],
+)
+def test_validate_time_string(time: str, expected: str):
+    result = validate_time_string(time)
+
+    assert result == expected
+
+
+@pytest.mark.parametrize("time", [("5m"), ("480h"), ("130")])
+def test_validate_time_raises_error(time: str):
+    with pytest.raises(ValueError):
+        validate_time_string(time)
+
+
+def test_get_version():
+    result = get_version()
+
+    assert result != "0.0.0"
+
+
+@pytest.mark.parametrize(
+    "seconds, expected",
+    [
+        ("1h", 3600),
+        ("45m", 2700),
+        ("65s", 65),
+        ("1h30m", 5400),
+    ],
+)
+def test_hms_to_seconds(seconds: str, expected: int):
+    result = hms_to_seconds(seconds)
+
+    assert result == expected
