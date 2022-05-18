485e9b53a419254826fb9172f15e3cf47e960e71
Matthew A. Knop <knopmatthew93@gmail.com>
2022-05-18 15:28:02 -0600

    Rhcloud 18846 use new labels annotations (#210)
    * Changed annotation check from 'status' to 'env-status'.
    
    * Changed 'status' to 'env-status'.
    
    * get_all_namespaces passes label to retrieve correct namespaces.
    
    * Added label to test data.
    
    * Added label property to namespace class.
    
    * Updated tests to reflect change.
    
    * Removed 'operator-ns' from annotations.
    
    * Fixed issues flake8 found.
    
    * Fixed cloud environments label name back to 'status'
    
    * Removed function return types.
    
    * Fixed reservation data 'env-status' back to 'status'.
    
    * Removed unused import.
    
    * Remove accidental merge conflict txt
    
    * Black formatting
    
    Co-authored-by: Matt Knop <mknop@redhat.com>
    Co-authored-by: Brandon Squizzato <bsquizza@redhat.com>

--- tests/test_bonfire.py
+++ tests/test_bonfire.py
@@ -11,13 +11,13 @@ DATA_PATH = Path(__file__).parent.joinpath("data")
 
 
 @pytest.fixture(scope="module")
-def namespace_list() -> list:
+def namespace_list():
     with open(DATA_PATH.joinpath("namespace_data.json"), "r") as namespace_data_file:
         return json.load(namespace_data_file)["items"]
 
 
 @pytest.fixture(scope="module")
-def reservation_list() -> list:
+def reservation_list():
     with open(DATA_PATH.joinpath("reservation_data.json"), "r") as reservation_data_file:
         return json.load(reservation_data_file)["items"]
 
