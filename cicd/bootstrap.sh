46638764e3ddfeedf6b3c6064eeb3a1a2526b98d
Brandon Tweed <btweed@redhat.com>
2023-02-06 16:25:50 -0600

    Tweak bootstrap to aid in debugging a specific issue

--- cicd/bootstrap.sh
+++ cicd/bootstrap.sh
@@ -42,7 +42,6 @@ export KUBECONFIG_DIR="$WORKSPACE/.kube"
 export KUBECONFIG="$KUBECONFIG_DIR/config"
 rm -fr $KUBECONFIG_DIR
 mkdir $KUBECONFIG_DIR
-set +x
 
 # if this is a PR, use a different tag, since PR tags expire
 if [ ! -z "$ghprbPullId" ]; then
@@ -66,6 +65,7 @@ export LC_ALL=en_US.utf-8
 python3 -m venv .bonfire_venv
 source .bonfire_venv/bin/activate
 
+set +x
 pip install --upgrade pip 'setuptools<58' wheel
 pip install --upgrade 'crc-bonfire>=4.10.4'
 
