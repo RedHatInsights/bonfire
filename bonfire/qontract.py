4168ee94c756ce02fce6b84b1bbf0ca275bfe5ef
Brandon Squizzato <35474886+bsquizz@users.noreply.github.com>
2023-04-11 10:08:31 -0400

    Handle deploy targets that have None namespace (#303)

--- bonfire/qontract.py
+++ bonfire/qontract.py
@@ -282,7 +282,7 @@ def get_apps_for_env(env_name):
         for saas_file in saas_files:
             for resource_template in saas_file.get("resourceTemplates", []):
                 for target in resource_template.get("targets", []):
-                    if target["namespace"]["name"] in env["namespaces"]:
+                    if target.get("namespace") and target["namespace"]["name"] in env["namespaces"]:
                         # this target belongs to the environment
                         _add_component(
                             apps,
