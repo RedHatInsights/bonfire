37b1e250e17b8ba820f97b0bf020e78c48fc10c4
Matt Holder <mjholder96@gmail.com>
2023-11-17 08:41:26 -0500

    indented continuation

--- bonfire/utils.py
+++ bonfire/utils.py
@@ -393,7 +393,7 @@ def get_dependencies(items, optional=False):
     for item in items:
         name = item.get("metadata", {}).get("name")
         bonfire_deps = item.get("metadata", {}).get("annotations", {}) \
-        .get("bonfire.dependencies", "").split(',')
+            .get("bonfire.dependencies", "").split(',')
         filtered_bonfire_deps = list(filter(lambda x: x != '', bonfire_deps))
         if name and filtered_bonfire_deps:
             deps_for_app[name].update(filtered_bonfire_deps)
