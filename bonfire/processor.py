2f196bde44c2c680ee98968975bf59e4e8aad3e6
Brandon Squizzato <bsquizza@redhat.com>
2023-12-21 15:03:40 -0500

    Fix comment

--- bonfire/processor.py
+++ bonfire/processor.py
@@ -245,7 +245,8 @@ def _should_remove(
     #   "--remove-option all" is set
     #   "--remove-option all --no-remove-option x" is set and app/component does NOT match 'x'
     #   "--no-remove-option all --remove-option x" is set and app/component matches 'x'
-    #   "--no-remove-option app1 --remove-option"
+    #   "--no-remove-option app1 --remove-option component" and component matches
+    #   none of the above conditions match and default=True
     remove_for_all_no_exceptions = remove_option.select_all and no_remove_option.empty
     remove_for_none_no_exceptions = no_remove_option.select_all and remove_option.empty
 
