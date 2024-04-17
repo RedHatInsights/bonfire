f1ca2f129d9b59ddf7e863c4299ccbbda55a054a
Matt Holder <mjholder96@gmail.com>
2024-04-17 15:47:31 -0400

    fix: includes cicd_tools_bootstrap.sh in tmp_job_dir for cleanup

--- cicd/bootstrap.sh
+++ cicd/bootstrap.sh
@@ -2,11 +2,12 @@
 
 set -e
 
+export TMP_JOB_DIR=${TMP_JOB_DIR:-$(mktemp -d -p "$HOME" -t "jenkins-${JOB_NAME}-${BUILD_NUMBER}-XXXXXX")}
 # which branch to fetch cicd scripts from in cicd-tools repo
 CICD_REPO_BRANCH="${CICD_REPO_BRANCH:-main}"
 CICD_REPO_ORG="${CICD_REPO_ORG:-RedHatInsights}"
 BOOTSTRAP_URL="https://raw.githubusercontent.com/${CICD_REPO_ORG}/cicd-tools/${CICD_REPO_BRANCH}/bootstrap.sh"
-BOOTSTRAP_FILE="./.cicd_tools_bootstrap.sh"
+BOOTSTRAP_FILE="${TMP_JOB_DIR}/.cicd_tools_bootstrap.sh"
 
 echo "Fetching $BOOTSTRAP_URL"
 rm -f $BOOTSTRAP_FILE
