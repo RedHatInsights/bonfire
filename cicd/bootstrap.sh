9c916863140b593abd36e8421fa881e686acbc06
mjholder <mjholder96@gmail.com>
2024-04-18 12:37:32 -0400

    Cleanup CICD tool scripts (#360)
    * source bootstrap file directly instead of writing to disk

--- cicd/bootstrap.sh
+++ cicd/bootstrap.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
 
-set -e
-
 # which branch to fetch cicd scripts from in cicd-tools repo
 CICD_REPO_BRANCH="${CICD_REPO_BRANCH:-main}"
 CICD_REPO_ORG="${CICD_REPO_ORG:-RedHatInsights}"
-BOOTSTRAP_URL="https://raw.githubusercontent.com/${CICD_REPO_ORG}/cicd-tools/${CICD_REPO_BRANCH}/bootstrap.sh"
-BOOTSTRAP_FILE="./.cicd_tools_bootstrap.sh"
+BOOTSTRAP_SCRIPT_URL="https://raw.githubusercontent.com/${CICD_REPO_ORG}/cicd-tools/${CICD_REPO_BRANCH}/bootstrap.sh"
 
-echo "Fetching $BOOTSTRAP_URL"
-rm -f $BOOTSTRAP_FILE
-RESPONSE_CODE=$(curl --silent -w "%{http_code}" -o "$BOOTSTRAP_FILE" "$BOOTSTRAP_URL")
-echo "HTTP response: $RESPONSE_CODE"
-source "$BOOTSTRAP_FILE"
+set -e
+source <(curl -sSL "$BOOTSTRAP_SCRIPT_URL")
+set +e 
