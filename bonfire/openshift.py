58e02d4f59b19bfd4d8f9a8a5aee8a9e5083aa55
wesgreen <wesgreen@redhat.com>
2025-10-21 15:17:01 -0400

    Fixing formatting issues

--- bonfire/openshift.py
+++ bonfire/openshift.py
@@ -389,9 +389,19 @@ def log_namespace_events(namespace, only_show_errors=False):
     """
     Retrieve and log namespace events.
     """
-    log.info("="*100)
+    log.info("=" * 100)
     log.info("Retrieving events from namespace '%s'...\n", namespace)
 
-    oc("get", "events", "-n", namespace, "--no-headers", "--field-selector", f"type={'Warning' if only_show_errors else 'Normal'}", _ok_code=[0, 1], _silent=False)
+    oc(
+        "get",
+        "events",
+        "-n",
+        namespace,
+        "--no-headers",
+        "--field-selector",
+        f"type={'Warning' if only_show_errors else 'Normal'}",
+        _ok_code=[0, 1],
+        _silent=False
+    )
 
-    log.info("="*100)
+    log.info("=" * 100)
